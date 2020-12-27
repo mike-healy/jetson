4ff055c650ffdda5200d68f298c8676d274dd7df
Mike Healy <design@mikehealy.com.au>
2020-12-28 08:15:47 +1000

    Initial Jetson based commit

---
+++ components/Masthead.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="appear-me">
+    <img
+    src="/at-pool_600.jpg"
+    alt="Jetson chillin at the pool"
+    srcset="/at-pool_600.jpg 600w, /at-pool_1300.jpg 1300w"
+    sizes="width: 400px"
+    class="circle">
+  </div>
+</template>
+
+<style>
+.appear-me {
+  animation: 1s appear;
+  margin: auto;
+  max-width: 400px;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+
+img.circle {
+  clip-path: circle();
+}
+</style>
